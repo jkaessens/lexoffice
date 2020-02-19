03e27c7b32742e63dd44dbbb0f5b0a497c9523fd
Enno Boland <g@s01.de>
2020-02-19 09:12:09 +0100

    add documentation to request/mod.rs

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -18,10 +18,29 @@ use crate::client::Client;
 use reqwest::Url;
 use std::marker::PhantomData;
 
+/// This crate is needed in order to build an URL for a request. It is implemented by
+/// all Request variants that allow Requests to the API.
+///
+/// If you want to reach an endpoint
+/// `https://api.lexoffice.io/foobar` you must implement this trait as follows:
+///
+/// ```
+/// // Model
+/// struct Foobar {
+///     // ...
+/// }
+/// impl Endpoint for Request<Foobar> {
+///     const ENDPOINT: &'static str = "foobar";
+/// }
+/// ```
 pub trait Endpoint {
+    /// The endpoint of a request.
     const ENDPOINT: &'static str;
 }
 
+/// Represents a request to the lexoffice API. Please note that in order to
+/// create a working `Request` the Type variable `T` must allow requests to be
+/// made.
 #[derive(Clone, Debug)]
 pub struct Request<T, S> {
     client: Client,
@@ -31,6 +50,7 @@ pub struct Request<T, S> {
 }
 
 impl<T, S> Request<T, S> {
+    /// Creates a new Request based with a Client
     pub fn new(client: Client) -> Self {
         let url = client.base_url().clone();
         Self {
